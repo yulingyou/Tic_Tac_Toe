0512c235091e5b77dbff12b6c20efa8cf04fa155
Daisy <ysiad926@gmail.com>
2023-03-30 19:42:25 +0100

    take_turn method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,19 @@
 class TicTacToe
   def initialize
     @board = Array.new(3) { Array.new(3, "") }
+    @current_player = "X"
   end
 
   def board
     @board
+  end
+
+  def take_turn(row, col)
+    if @board[row][col] == ""
+      @board[row][col] = @current_player
+      return true
+    else
+      return false
+    end
   end
 end
